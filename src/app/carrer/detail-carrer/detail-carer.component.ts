cd2d14ea0eb1e60be107c0b1d73dd00d363df640
THUY-TRAN <phuongthuy791997@gmail.com>
2019-05-25 14:59:42 +0700

    detail carrer

---
+++ src/app/carrer/detail-carrer/detail-carer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-carer',
+  templateUrl: './detail-carer.component.html',
+  styleUrls: ['./detail-carer.component.scss']
+})
+export class DetailCarerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
